d52838ba4a6ca8ce0eb89dc2b713fe544748c106
megamindreb <umateshubham7@gmail.com>
2021-11-12 23:39:16 +0530

    added complete mod

--- 23mod/main.go
+++ 23mod/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main()  {
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
 	fmt.Println("Hello mod in golang")
+	greeter()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func greeter() {
+	fmt.Println("Hey there mod users")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to golang series on youtube</h1>"))
 }
