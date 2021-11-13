dfe25c5e1f99114e86c595af669b6c0dcf1569be
shubham-umate <umateshubham7@gmail.com>
2021-11-13 21:57:59 +0530

    concurrency, waitGroup and mutex

--- 26goroutine/main.go
+++ 26goroutine/main.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup // pointer
+
+var signals = []string{"test"}
+
+var mut sync.Mutex
 
 func main() {
 	fmt.Println("Goroutine")
 
-	go greeter("Hello")
-	greeter("World")
+	websiteList := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+	}
+
+	for _, v := range websiteList {
+		go getStatusCode(v)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+
+	fmt.Println(signals)
+	//go greeter("Hello")
+	//greeter("World")
 }
 
 func greeter(s string) {
@@ -18,5 +40,21 @@ func greeter(s string) {
 		time.Sleep(2 * time.Second)
 		fmt.Println(s)
 	}
+
+}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("Oops in endpoint...")
+	}
+
+	mut.Lock()
+	signals = append(signals, endpoint)
+	mut.Unlock()
+	fmt.Printf("%d Status code for %s\n", res.StatusCode, endpoint)
 
 }
